94cf5c85438c231bc102828efab68e7944c510a1
Denny_Kate <dennykate22@gmail.com>
2023-04-21 22:38:23 +0630

    uninstall bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 import User from "../models/users.js";
-import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export const register = async (req, res, next) => {
   try {
@@ -12,7 +12,10 @@ export const register = async (req, res, next) => {
         .json({ status: false, message: "Email already taken" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = crypto
+      .createHash("md5")
+      .update(password)
+      .digest("hex");
 
     const user = await User.create({
       username,
@@ -32,19 +35,24 @@ export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+    const hashedPassword = crypto
+      .createHash("md5")
+      .update(password)
+      .digest("hex");
+
+    const user = await User.findOne({ email, password: hashedPassword });
     if (!user) {
       return res
         .status(400)
-        .json({ message: "Incorrect Email", status: false });
+        .json({ message: "Incorrect Email or Password", status: false });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res
-        .status(400)
-        .json({ message: "Incorrect Password", status: false });
-    }
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+    // if (!isPasswordValid) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Incorrect Password", status: false });
+    // }
 
     delete user.password;
     return res.status(200).json({ status: true, user });
